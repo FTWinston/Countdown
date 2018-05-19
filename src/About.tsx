16394c16402b459fb05bf0f90881698121eefc98
FTWinston <ftwinston@gmail.com>
2018-05-19 13:37:33 +0100

    About screen text now scrolls when needed

--- src/About.tsx
+++ src/About.tsx
@@ -18,9 +18,9 @@ export class About extends React.PureComponent<IAboutProps, {}> {
                     <p><a href="http://www.channel4.com/programmes/countdown">Countdown</a> is a long-running British
                     TV game show, involving word and number puzzles. It has been running since 1982.</p>
                     <p>This version forced its way out of my brain over a weekend in May 2018, when I really should
-                    have been focusing on more important things. It is free,
-                    <a href="https://github.com/FTWinston/Countdown">open source</a> and you're welcome to contribute.
-                    </p>
+                    have been focusing on more important things. 
+                    It is free, <a href="https://github.com/FTWinston/Countdown">open source</a> and you're welcome
+                    to contribute.</p>
                     <p><strong>Letters games</strong> have a player randomly pick letters (by choosing either
                     consonants or vowels, one at a time), and then attempting to form the longest-possible word
                     from those letters. The player with the longest <em>valid</em> word when the time runs out
