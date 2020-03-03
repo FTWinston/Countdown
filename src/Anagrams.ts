ae606b25ed69163b0ddecfc0c8ad3586380fec0a
FTWinston <ftwinston@gmail.com>
2020-03-03 22:03:52 +0000

    Changed to different CORS proxy

--- src/Anagrams.ts
+++ src/Anagrams.ts
@@ -1,7 +1,7 @@
 export async function findWords(letters: string, maxResults: number) {
     // if you feed this letters that exactly match a word, this API method only returns that word.
     // this works around that by adding a . on the end
-    const response = await fetch(`https://cors-proxy.htmldriven.com/?url=http://www.anagramica.com/all/${letters.toLowerCase()}.`);
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/http://anagramica.com/all/${letters.toLowerCase()}.`);
     const data = await response.json();
     
     const allWords = JSON.parse(data.body).all as string[];
