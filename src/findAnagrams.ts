2a9a675536c469e532a9e0a6390ab6ebaec4aef5
FTWinston <ftwinston@gmail.com>
2022-07-23 23:13:28 +0100

    Various tweaks

--- src/findAnagrams.ts
+++ src/findAnagrams.ts
@@ -1,13 +1,17 @@
 import fetch from 'node-fetch';
 
+interface ApiResponse {
+    all: string[];
+}
+
 export async function findAnagrams(letters: string, maxResults: number) {
     // if you feed this letters that exactly match a word, this API method only returns that word.
     // this works around that by adding a . on the end
     const apiResponse = await fetch(`http://anagramica.com/all/${letters.toLowerCase()}.`);
-    const apiData = await apiResponse.json();
-    const allWords = (apiData as any).all as string[];
+    const apiData = await apiResponse.json() as ApiResponse;
+    const allWords = apiData.all;
     
     // If we have more than the requested number of words, trim superfluous ones. Convert to upper case.
-    return (allWords.length < maxResults ? allWords : allWords.slice(0, maxResults))
+    return ((maxResults <= 0 || allWords.length < maxResults) ? allWords : allWords.slice(0, maxResults))
         .map(word => word.toUpperCase())
 }
