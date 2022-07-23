b4f75f388dac15d91d4bf6da25d2bb4b1740f07a
FTWinston <ftwinston@gmail.com>
2022-07-23 21:07:08 +0100

    Letters game now calls netlify function

--- src/Anagrams.ts
+++ src/Anagrams.ts
@@ -1,11 +1,14 @@
 export async function findWords(letters: string, maxResults: number) {
     // if you feed this letters that exactly match a word, this API method only returns that word.
     // this works around that by adding a . on the end
-    const response = await fetch(`https://cors.bridged.cc/http://anagramica.com/all/${letters.toLowerCase()}.`);
-    const data = await response.json();
-    
-    const allWords = data.all as string[];
-    const useWords = allWords.length < maxResults ? allWords : allWords.slice(0, maxResults);
+    const response = await fetch(`/.netlify/functions/findWords/${letters.toLowerCase()}.`, {
+        method: 'POST'
+    });
 
-    return useWords.map(w => w.toUpperCase());
+    if (!response.ok) {
+        return [];
+    }
+
+    const data: string[] = await response.json();
+    return data;
 }
