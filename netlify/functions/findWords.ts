b711986e7f3f608e8e8480a0f4b5e524f34f1a99
FTWinston <ftwinston@gmail.com>
2022-07-23 20:09:20 +0100

    Added a findWords netlify function

---
+++ netlify/functions/findWords.ts
@@ -0,0 +1,32 @@
+import { Handler } from '@netlify/functions';
+
+// Returns the 3 longest anagrams found by scrambling the "letters" query parameter. 
+const handler: Handler = async (event, context) => {
+    const letters = event.queryStringParameters?.letters?.toLowerCase() ?? null;
+
+    if (!letters) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: '"letters" query string parameter expected"' }),
+        };
+    }
+
+    // if you feed this letters that exactly match a word, this API method only returns that word.
+    // this works around that by adding a . on the end
+    const apiResponse = await fetch(`https://cors.bridged.cc/http://anagramica.com/all/${letters}.`);
+    const apiData = await apiResponse.json();
+
+    const allWords = apiData.all as string[];
+    
+    const maxResults = 3;
+
+    const responseWords = (allWords.length < maxResults ? allWords : allWords.slice(0, maxResults))
+        .map(word => word.toUpperCase())
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(responseWords),
+    };
+};
+
+export { handler };
